b4bcaea603d9ec3defc8af89fa2682a07cd8a5db
ronpysui <kevinnguyen602641@gmail.com>
2022-04-12 14:37:52 -0500

    4/12/22

---
+++ Assets/Scripts/ItemHandlerScripts/ItemDropFunction.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropFunction : MonoBehaviour
+{
+    public void Update(){
+        
+    }
+}
--- Assets/Scripts/UI Scripts/InventoryController.cs
+++ Assets/Scripts/UI Scripts/InventoryController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class InventoryController: MonoBehaviour
 {
+    public List<GameObject>inventorySlots=new List<GameObject>();
     public List<string>items;
     public List<bool>invCheck;
     public bool canCollide;
@@ -15,7 +16,12 @@ public class InventoryController: MonoBehaviour
         {"pumkinseed",0},
         {"milk",0},
     };
-    private void Start(){
+    private void Awake(){
+        inventorySlots.Add(GameObject.Find("InvSlot_1"));
+        inventorySlots.Add(GameObject.Find("InvSlot_2"));
+        inventorySlots.Add(GameObject.Find("InvSlot_3"));
+        inventorySlots.Add(GameObject.Find("InvSlot_4"));
+        inventorySlots.Add(GameObject.Find("InvSlot_5"));
     }
     private void Update(){
         if(items.Count>0){//if theres any items in item list
@@ -27,4 +33,7 @@ public class InventoryController: MonoBehaviour
             }
         }
     }
+
+    //inventory animation
+
 }
