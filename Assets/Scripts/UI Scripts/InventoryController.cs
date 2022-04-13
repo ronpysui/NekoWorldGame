3597e9d2a6160f6df1cc6004c1758416e1a6939f
ronpysui <kevinnguyen602641@gmail.com>
2022-04-13 00:57:58 -0500

    4/12/22

--- Assets/Scripts/UI Scripts/InventoryController.cs
+++ Assets/Scripts/UI Scripts/InventoryController.cs
@@ -7,22 +7,20 @@ using UnityEngine;
 public class InventoryController: MonoBehaviour
 {
     public List<GameObject>inventorySlots=new List<GameObject>();
+    public Sprite invSelectSprite;
+    public Sprite invSprite;
     public List<string>items;
     public List<bool>invCheck;
     public bool canCollide;
+    public int scrollposition;
+    public int prevscrollposition;
     public Dictionary<string,int>nonStackableItemsContainer=new Dictionary<string, int>(){
     };
     public Dictionary<string,int>StackableItemsContainer=new Dictionary<string, int>(){
         {"pumkinseed",0},
+        {"carrotseed",0},
         {"milk",0},
     };
-    private void Awake(){
-        inventorySlots.Add(GameObject.Find("InvSlot_1"));
-        inventorySlots.Add(GameObject.Find("InvSlot_2"));
-        inventorySlots.Add(GameObject.Find("InvSlot_3"));
-        inventorySlots.Add(GameObject.Find("InvSlot_4"));
-        inventorySlots.Add(GameObject.Find("InvSlot_5"));
-    }
     private void Update(){
         if(items.Count>0){//if theres any items in item list
             for(int i=0;i<items.Count;i++){//loop
@@ -32,8 +30,29 @@ public class InventoryController: MonoBehaviour
                 //update each inventory slot
             }
         }
+        //0-
+        if(Input.GetAxis("Mouse ScrollWheel")>0f){
+            if(scrollposition==1){
+                GameObject.Find("InvSlot_1").GetComponent<UnityEngine.UI.Image>().sprite=invSelectSprite;
+            }
+            else{
+                if(scrollposition<6){
+                    prevscrollposition++;
+                    GameObject.Find("InvSlot_"+scrollposition).GetComponent<UnityEngine.UI.Image>().sprite=invSelectSprite;
+                    GameObject.Find("InvSlot_"+(prevscrollposition-1)).GetComponent<UnityEngine.UI.Image>().sprite=invSprite;
+                }
+            }
+            if(scrollposition<6){
+                scrollposition++;
+            }
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel")<0f){
+            if(prevscrollposition>1){
+                scrollposition--;
+                prevscrollposition--;
+                GameObject.Find("InvSlot_"+scrollposition).GetComponent<UnityEngine.UI.Image>().sprite=invSprite;
+                GameObject.Find("InvSlot_"+(prevscrollposition)).GetComponent<UnityEngine.UI.Image>().sprite=invSelectSprite;
+            }
+        }
     }
-
-    //inventory animation
-
 }
